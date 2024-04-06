c487611d33faaee4ec102fb61cf035172f976835
Diogo SIlva <diogosilvaoficial@gmail.com>
2024-04-05 21:43:21 -0300

    Webserver

---
+++ adapters/web/handler/product.go
@@ -0,0 +1,33 @@
+package handler
+
+import (
+	"encoding/json"
+	"github.com/codegangsta/negroni"
+	"github.com/diogosilvasantana/go-hexagonal/application"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+func MakeProductHandler(r *mux.Router, n *negroni.Negroni, service application.ProductServiceInterface) {
+	r.Handle("/products/{id}", n.With(
+		negroni.Wrap(getProduct(service)),
+	)).Methods("GET", "OPTIONS")
+}
+
+func getProduct(service application.ProductServiceInterface) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		vars := mux.Vars(r)
+		id := vars["id"]
+		product, err := service.Get(id)
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+		err = json.NewEncoder(w).Encode(product)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	})
+}
---
+++ adapters/web/server/server.go
@@ -0,0 +1,43 @@
+package server
+
+import (
+	"github.com/codegangsta/negroni"
+	"github.com/diogosilvasantana/go-hexagonal/adapters/web/handler"
+	"github.com/diogosilvasantana/go-hexagonal/application"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+type WebServer struct {
+	Service application.ProductServiceInterface
+}
+
+func MakeNewWebServer(service application.ProductServiceInterface) WebServer {
+	return WebServer{Service: service}
+}
+
+func (w WebServer) Serve() {
+
+	r := mux.NewRouter()
+	n := negroni.New(
+		negroni.NewLogger(),
+	)
+
+	handler.MakeProductHandler(r, n, w.Service)
+	http.Handle("/", r)
+
+	server := &http.Server{
+		ReadHeaderTimeout: 10 * time.Second,
+		WriteTimeout:      10 * time.Second,
+		Addr:              ":8080",
+		Handler:           nil,
+		ErrorLog:          log.New(os.Stderr, "log: ", log.Lshortfile),
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
